6fb3709cd2d79ce0d06f84d11c68e118d73f33ca
BharadwajPolusani <bharadwajpolusani1362@gmail.com>
2023-12-04 10:52:15 -0600

    Adding Generated File

---
+++ WP-VB-ToDoApplication/src/main/java/com/VB/WPVBToDoApplication/WpVbToDoApplication.java
@@ -0,0 +1,13 @@
+package com.VB.WPVBToDoApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpVbToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WpVbToDoApplication.class, args);
+	}
+
+}
---
+++ WP-VB-ToDoApplication/src/test/java/com/VB/WPVBToDoApplication/WpVbToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VB.WPVBToDoApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpVbToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
